05f7a55d86d9e040598779c186c9aeb8eb09d60a
Konrad Karanowski <55788735+konrad-karanowski@users.noreply.github.com>
2020-02-28 20:58:16 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+from frames import MainFrame
+from styles import style_config
+
+
+# all frames in application
+FRAMES = {
+    'main': MainFrame
+}
+
+
+class Application(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        # basic configs
+        self.title('Metronome')
+        self.geometry('300x200')
+        self.resizable(False, False)
+
+        # config styles using custom function
+        style_config(self)
+
+        # main container for frames
+        container = ttk.Frame(self)
+        container.grid()
+
+        # all frames in app
+        self.frames = dict()
+        for key, frame in FRAMES.items():
+            self.frames[key] = frame(container, self)
+            self.frames[key].grid(row=0, column=0, sticky='nsew')
+
+        self.show_frame('main')
+
+    def show_frame(self, key):
+        """
+        Switch between the frames
+        :param key: key in dict
+        :return:
+        """
+        frame = self.frames[key]
+        frame.tkraise()
---
+++ frames/main_frame.py
@@ -0,0 +1,249 @@
+import tkinter as tk
+from tkinter import ttk
+from pygame import mixer
+import time
+import os
+
+current_dir = os.path.abspath(os.getcwd())
+base_path = os.path.join(current_dir, 'frames/sounds')
+
+ACCENT_SOUND = os.path.join(base_path, 'accent.mp3')
+NORMAL_SOUND = os.path.join(base_path, 'normal.mp3')
+print(os.path.isfile(ACCENT_SOUND))
+
+
+class MainFrame(ttk.Frame):
+
+    def __init__(self, container, controller, **kwargs):
+        super().__init__(container, **kwargs)
+        self.grid_rowconfigure(0, minsize=100)
+        self.grid_columnconfigure(0, minsize=270)
+
+        # variables
+
+        # beats per minute
+        self.bpm = tk.IntVar(value=80)
+        # counts (num of notes in one beat)
+        self.counts = tk.IntVar(value=4)
+        # is metronome running
+        self.is_running = tk.BooleanVar(value=False)
+
+        # list of ticks
+        self.ticks = list()
+
+        # containers
+
+        # container for counts
+        self.count_container = ttk.Frame(self)
+        self.count_container.grid(row=0, column=0, sticky='')
+        self.create_ticks(event=None)
+
+        # container for bpm settings
+        self.bpm_container = ttk.Frame(self)
+        self.bpm_container.grid(row=1, column=0, sticky='')
+        self.bpm_container.grid_columnconfigure(1, minsize=100)
+        self.create_bpm_settings()
+
+        # container for addition tools and buttons
+        self.tools_container = ttk.Frame(self)
+        self.tools_container.grid(row=2, column=0, sticky='')
+        self.create_tools()
+
+    def create_ticks(self, event):
+        """
+        Create all ticks in app
+        :return:
+        """
+        # removing all previous ticks
+        for tick in self.count_container.grid_slaves():
+            tick.destroy()
+
+        # creating new list of ticks
+        self.ticks = list()
+
+        ticks_left = self.counts.get()
+        total = 0
+        rows = 1 if ticks_left <= 8 else 2
+        for i in range(rows):
+            for j in range(8):
+                if total != ticks_left:
+                    new_tick = tk.Canvas(
+                        self.count_container,
+                        width=32,
+                        height=32,
+                    )
+                    new_tick.create_oval(1, 1, 31, 31, outline='', fill='yellow')
+                    new_tick.grid(row=i, column=j, sticky='nsew')
+                    self.ticks.append(new_tick)
+                    total += 1
+                else:
+                    break
+
+        # change first tick to orange (accent on first note)
+        self.ticks[0].itemconfig(1, fill='orange')
+
+    def create_bpm_settings(self):
+        """
+        Create all widgets in bpm setting container
+        :return:
+        """
+        self.bpm_label = ttk.Label(
+            self.bpm_container,
+            textvariable=self.bpm,
+            anchor='center'
+        )
+        self.bpm_label.grid(row=0, column=1, sticky='ew')
+
+        # 11
+        self.plus = ttk.Button(
+            self.bpm_container,
+            command=lambda: self.update_bpm(1),
+        )
+        self.plus.grid(row=0, column=2, sticky='e')
+
+        self.minus = ttk.Button(
+            self.bpm_container,
+            command=lambda: self.update_bpm(-1)
+        )
+        self.minus.grid(row=0, column=0, sticky='w')
+
+        self.slider = tk.Scale(
+            self.bpm_container,
+            from_=1,
+            to=300,
+            orient='horizontal',
+            showvalue=False,
+            variable=self.bpm
+        )
+        self.slider.grid(row=1, column=0, columnspan=3, sticky='ew')
+
+    def create_tools(self):
+        """
+        Create all other tools for metronome
+        :return:
+        """
+        self.start_button = ttk.Button(
+            self.tools_container,
+            text='Start',
+            command=self.start
+        )
+        self.start_button.grid(row=0, column=1, sticky='ew')
+
+        self.stop_button = ttk.Button(
+            self.tools_container,
+            text='Stop',
+            command=self.stop
+        )
+        self.stop_button.grid(row=0, column=2, sticky='ew')
+
+        self.counts_combobox = ttk.Combobox(
+            self.tools_container,
+            values=[i + 1 for i in range(16)],
+            textvariable=self.counts,
+            state='readonly',
+            width=8
+        )
+        self.counts_combobox.grid(row=0, column=0, sticky='w')
+        self.counts_combobox.bind('<<ComboboxSelected>>', self.create_ticks)
+
+    def update_bpm(self, change):
+        """
+        Updates self.bpm variable
+        :param change: change in bpm
+        :return:
+        """
+        new_bpm = self.bpm.get() + change
+        if 1 <= new_bpm <= 300:
+            self.bpm.set(new_bpm)
+
+    def start(self):
+        """
+        Start or stop working of metronome
+        :return:
+        """
+        self.is_running.set(True)
+        self.start_button.configure(state='disabled')
+        self.stop_button.configure(state='enabled')
+        # disable option to change count numbers while metronome is running
+        self.counts_combobox.unbind('<<ComboboxSelected>>')
+
+        # start counting
+        self.play(tick=self.counts.get() -1)
+
+    def stop(self):
+        """
+        Stops the timer
+        :return:
+        """
+        self.is_running.set(False)
+        self.stop_button.configure(state='disabled')
+        self.start_button.configure(state='enabled')
+        # sets all ticks colours to default
+        self.reset_ticks()
+        # bind combobox again
+        self.counts_combobox.bind('<<ComboboxSelected>>', self.create_ticks)
+
+    def play(self, tick):
+        """
+        Counting process
+        :param tick: index of a previous item
+        :return:
+        """
+        # counting loop
+        if self.is_running.get():
+            start_time = time.time()
+            # changing previous tick to default color:
+            self.change_tick(tick)
+
+            time_to_wait = int(60 / self.bpm.get() * 1000)
+
+            # updating tick index to current
+            if tick + 1 == self.counts.get():
+                tick = 0
+            else:
+                tick += 1
+
+            self.play_sound(tick)
+
+            # change color temporary to red
+            self.ticks[tick].itemconfigure(1, fill='red')
+
+            # after time of beat, go to next tick
+            new_time = time.time() - start_time
+            print(new_time)
+            self.after(
+                time_to_wait,
+                lambda: self.play(tick)
+            )
+
+    def change_tick(self, tick):
+        """
+        Change tick back to original colour
+        :param tick: index of tick
+        :return:
+        """
+        if tick == 0:
+            self.ticks[tick].itemconfigure(1, fill='orange')
+        else:
+            print(tick)
+            self.ticks[tick].itemconfigure(1, fill='yellow')
+
+    def reset_ticks(self):
+        """
+        Reset all ticks colours to default
+        :return:
+        """
+        self.ticks[0].itemconfigure(1, fill='orange')
+        for tick in self.ticks[1:]:
+            tick.itemconfigure(1, fill='yellow')
+
+    def play_sound(self, tick):
+        """
+        Plays metronome sound
+        :param tick: index of tick
+        :return:
+        """
+        if tick == 0:
+            pass
+        else:
+            pass
